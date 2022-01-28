b91eeb1ccbdd8722cf91284c63333928b6a79ade
MachariaCollins <86539347+MachariaCollins@users.noreply.github.com>
2022-01-28 19:12:03 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -2,4 +2,4 @@
 base=int(input("Enter the base:"))
 height=int(input("enter the height:"))
 area=(base*height)/2
-print("The area is the triangle is",area)
+print("The area of the triangle is",area)
